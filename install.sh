e359748308fba15e64f5ca32d8b24d388cddd26e
aviad2020 <aviadhahami@gmail.com>
2020-01-19 14:53:33 +0200

    done

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 #Install general deps
 echo "Installing general stuff"
-sudo apt-get install git curl
+sudo apt-get install git curl build-essential
 
 #Install NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
